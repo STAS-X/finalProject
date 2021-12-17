db5adf6c19694315b1106ee2999f417c3b031b66
STAS-X <91072180+STAS-X@users.noreply.github.com>
2021-12-17 10:25:26 +0300

    Update App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Users from "./components/users";
 import SearchStatus from "./components/searchStatus";
 import api from "./api";
